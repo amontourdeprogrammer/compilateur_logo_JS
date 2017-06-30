0329addbf3ac4900f05945e8c0154ebde9b8ebcc
Elsa Bourgeois <elsa31@gmail.com>
2017-06-30 12:46:24 +0200

    create prompt to get user input as logo command

--- cli.js
+++ cli.js
@@ -1,10 +1,33 @@
 var compilateur = require('./compilateurLogo.js');
 var fs = require('fs');
 var content = require('./template.js');
+var prompt = require('prompt');
+var userInput = [];
+// var inputFile = process.argv[2];
+//
+// fs.readFile(inputFile, 'utf8', function(err, codeLogo) {
+// 	var instructions = compilateur.CompilateurLogo(codeLogo);
+// 	console.log(content(instructions));
+// });
 
-var inputFile = process.argv[2];
+	var schema = {
+		properties : {
+			mot: {
+				pattern: /^[a-z]/,
+				message: "must be 'av' or 'td'",
+				required: true
+			},
+			pas: {
+				pattern: /^[1-200]/,
+				message: "must be a number between 1 and 200 ",
+				required: true
+			},
+		}
+	};
 
-fs.readFile(inputFile, 'utf8', function(err, codeLogo) {
-	var instructions = compilateur.CompilateurLogo(codeLogo);
-	console.log(content(instructions));
-});
+	prompt.start();
+	prompt.get(schema, function (err, result){
+		userInput.push(result.mot+' '+ result.pas);
+		console.log(result.mot);
+
+	});
